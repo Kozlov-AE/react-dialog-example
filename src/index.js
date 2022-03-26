cb2663e602606aa3bd6c9728777ad9e5ed7f9d31
Kozlov-AE <Allek-007@list.ru>
2022-03-26 15:01:45 +0300

    FromSite

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import WebFontLoader from "webfontloader";
+import App from "./App";
+import "./css/index.css";
+
+WebFontLoader.load({
+  google: {
+    families: ["Open Sans:300,400,700"],
+  },
+});
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
